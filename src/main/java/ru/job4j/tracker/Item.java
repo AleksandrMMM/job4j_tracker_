d94ace951ce3a0657f403e43078324437a82331c
AleksMMM <gol8an@yandex.ru>
2021-09-09 10:14:22 +0300

    Date. Отображение даты.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(int id) {
         this.id = id;
@@ -24,6 +27,10 @@ public class Item {
 
     public int getId() {
         return id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void setId(int id) {
