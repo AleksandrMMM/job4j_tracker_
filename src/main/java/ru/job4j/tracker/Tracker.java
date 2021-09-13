d36f420f7bfd266c57475bf63ae9b10b43aecae5
AleksMMM <gol8an@yandex.ru>
2021-09-13 17:27:04 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +24,36 @@ public class Tracker {
         }
         return rsl;
     }
-}
+
+
+    public Item[] findAll(Item[] item) {
+        Item[] ItemWithoutNull = new Item[item.length];
+        for (int index = 0; index < item.length; index++) {
+            Item itemNot = item[index];
+            if (itemNot != null) {
+                ItemWithoutNull[size] = itemNot;
+                size++;
+            }
+        }
+        ItemWithoutNull = Arrays.copyOf(ItemWithoutNull, size);
+        return ItemWithoutNull;
+
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        Item[] itemsFindName = new Item[items.length];
+        for (int i = 0; i < this.items.length; i++) {
+            if (items[i] != null) {
+                if (items[i].getName().equals(key)) {
+                    itemsFindName[i] = items[i];
+                    count++;
+                }
+            }
+        }
+        itemsFindName = Arrays.copyOf(itemsFindName, count);
+        return itemsFindName;
+
+    }
+
+ }
