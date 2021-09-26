bcb66a2ea8b2e155d1fb403bf649861f9c15cf4f
AleksMMM <gol8an@yandex.ru>
2021-09-26 14:47:08 +0300

    Кидаем исключение - throw new RuntimeException

---
+++ src/main/java/ex/Fact.java
@@ -0,0 +1,21 @@
+package ex;
+
+public class Fact {
+
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+}
