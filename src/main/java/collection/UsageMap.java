102f7c4555dbccf6926ae1b06ab8c265b51c308a
aleks <gol8an@yandex.ru>
2022-01-17 21:31:32 +0300

    0. Map, HashMap

---
+++ src/main/java/collection/UsageMap.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+}
