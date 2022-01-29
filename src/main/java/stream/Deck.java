769821251203d1adbd3f30aea54ac57a01b5bb50
aleks <gol8an@yandex.ru>
2022-01-30 01:11:15 +0300

    8. Перенос методов в Stream API

--- src/main/java/stream/Deck.java
+++ src/main/java/stream/Deck.java
@@ -3,10 +3,14 @@ package stream;
 import java.util.stream.Stream;
 
 public class Deck {
+    Suit[] levels = Suit.values();
+    Value[] tasks = Value.values();
     public static void main(String[] args) {
+        Suit[] levels = Suit.values();
+        Value[] tasks = Value.values();
         Stream.of(Suit.values())
-                .flatMap(sv -> Stream.of(Value.values()))
-                .map(value -> new Card(sv, value))
+                .flatMap(level -> Stream.of(Value.values())
+                        .map(task -> new Card(level, task)))
                 .forEach(System.out::println);
     }
 }
