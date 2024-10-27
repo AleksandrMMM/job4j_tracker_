bea441c2a5fcf66ceae0da4862caaad89200f904
Aleks <girbov@gmail.com>
2024-10-27 17:15:24 +0300

    Создание объекта

---
+++ src/main/java/oop/Dog.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class Dog {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+        Dog polkan = new Dog("polkan");
+        Dog sharik = new Dog("sharik");
+        Dog zhychka = new Dog("zhychka");
+    }
+}
